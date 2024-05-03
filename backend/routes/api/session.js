61a9046ec251a2ae47c432c79108ec8516c33b62
yinjiao <fyj121322@gmail.com>
2024-05-03 15:01:32 -0700

    validate signup

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const { Op } = require('sequelize');
 const bcrypt = require('bcryptjs');
-// const { check } = require('express-validator');
+const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
 
 const { setTokenCookie, restoreUser } = require('../../utils/auth');
@@ -10,21 +10,21 @@ const { User } = require('../../db/models');
 const router = express.Router();
 
 
-// const validateLogin = [
-//   check('credential')
-//     .exists({ checkFalsy: true })
-//     .notEmpty()
-//     .withMessage('Email or username is required'),
-//   check('password')
-//     .exists({ checkFalsy: true })
-//     .withMessage('Password is required'),
-//   handleValidationErrors
-// ];
+const validateLogin = [
+  check('credential')
+    .exists({ checkFalsy: true })
+    .notEmpty()
+    .withMessage('Email or username is required'),
+  check('password')
+    .exists({ checkFalsy: true })
+    .withMessage('Password is required'),
+  handleValidationErrors
+];
 
 // Log in
 router.post(
     '/',
-    // validateLogin,
+    validateLogin,
     async (req, res, next) => {
       const { credential, password } = req.body;
 
--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const bcrypt = require('bcryptjs');
-// const { check } = require('express-validator');
-// const { handleValidationErrors } = require('../../utils/validation');
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
 
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
 const { User } = require('../../db/models');
@@ -9,36 +9,36 @@ const e = require('express');
 
 const router = express.Router();
 
-// const validateSignup = [
-//   check('email')
-//     .exists({ checkFalsy: true })
-//     .isEmail()
-//     .withMessage('Invalid email'),
-//   check('username')
-//     .exists({ checkFalsy: true })
-//     .isLength({ min: 4 })
-//     .withMessage('Username is required'),
-//   check('username')
-//     .not()
-//     .isEmail()
-//     .withMessage('Username cannot be an email.'),
-//   check('password')
-//     .exists({ checkFalsy: true })
-//     .isLength({ min: 6 })
-//     .withMessage('Password must be 6 characters or more.'),
-//   check('firstName')
-//     .exists({ checkFalsy: true })
-//     .withMessage('First Name is required'),
-//   check('lastName')
-//     .exists({ checkFalsy: true })
-//     .withMessage('Last Name is required'),
-//   handleValidationErrors
-// ];
+const validateSignup = [
+  check('email')
+    .exists({ checkFalsy: true })
+    .isEmail()
+    .withMessage('Invalid email'),
+  check('username')
+    .exists({ checkFalsy: true })
+    .isLength({ min: 4 })
+    .withMessage('Username is required'),
+  check('username')
+    .not()
+    .isEmail()
+    .withMessage('Username cannot be an email.'),
+  check('password')
+    .exists({ checkFalsy: true })
+    .isLength({ min: 6 })
+    .withMessage('Password must be 6 characters or more.'),
+  check('firstName')
+    .exists({ checkFalsy: true })
+    .withMessage('First Name is required'),
+  check('lastName')
+    .exists({ checkFalsy: true })
+    .withMessage('Last Name is required'),
+  handleValidationErrors
+];
 
 // Sign up
 router.post(
     '/',
-    // validateSignup,
+    validateSignup,
     async (req, res, next) => {
       const { email, firstName, lastName, password, username } = req.body;
       const hashedPassword = bcrypt.hashSync(password);
