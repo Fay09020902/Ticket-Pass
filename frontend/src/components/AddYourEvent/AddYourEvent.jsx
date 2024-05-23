d835d662132042a96175605064307a09f2a4755f
yinjiao <fyj121322@gmail.com>
2024-05-22 20:04:06 -0700

    add set error empty for add event

--- frontend/src/components/AddYourEvent/AddYourEvent.jsx
+++ frontend/src/components/AddYourEvent/AddYourEvent.jsx
@@ -26,6 +26,7 @@ const AddYourEvent = () => {
     const dispatch = useDispatch();
     const addNewEvent = async (e) => {
         e.preventDefault();
+        setErrors({}); 
         const seatConfig = {rows, seatsPerRow}
         const newEvent = {
             name,
@@ -41,6 +42,7 @@ const AddYourEvent = () => {
             price,
             "ticketavailability": true
         }
+        // console.log('Submitting new event:', newEvent); // Debug log
 
         try {
             const response = await dispatch(addEventThunk(newEvent, seatConfig));
